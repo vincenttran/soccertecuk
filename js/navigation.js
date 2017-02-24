720c3c8800ef5108c061b21294aabc7d4fd09ade
Dominic Hickey <dominic.j.hickey@gmail.com>
2017-02-24 17:50:05 +0000

    nav added

--- js/navigation.js
+++ js/navigation.js
@@ -9,3 +9,8 @@ $(document).each(function() {
         .toggleClass('folder-open');
     });
 });
+
+// Toggles class open to display/hide the whole navigation
+$(document).on("click", ".navigation-toggle", function() {
+    $( ".bk-extendednavigation" ).toggleClass( "open" );
+});
